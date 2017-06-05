697f36cf7874b1ae7c82870e8dfb20c6c4ef0160
Eike Spielberg <Eike.Spielberg@uni-due.de>
2017-06-05 15:23:47 +0200

    initial commit

---
+++ src/main/java/unidue/ub/services/xmlrepository/XmlRepositoryApplication.java
@@ -0,0 +1,12 @@
+package unidue.ub.services.xmlrepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlRepositoryApplication.class, args);
+	}
+}
